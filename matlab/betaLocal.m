00f59d8fff428a0ac21badbe82d98acba39c6101
Daniel Larremore <daniel.larremore@gmail.com>
2017-09-15 16:41:00 -0600

    betaLocal

---
+++ matlab/betaLocal.m
@@ -0,0 +1,42 @@
+% SpringRank
+% CODE  ->  https://github.com/cdebacco/SpringRank
+% PAPER ->  http://danlarremore.com/pdf/SpringRank_2017_PrePrint.pdf
+% Code by Daniel Larremore
+% University of Colorado at Boulder
+% BioFrontiers Institute & Dept of Computer Science
+% daniel.larremore@colorado.edu
+% http://danlarremore.com
+%
+% b = betaLocal(A,s)
+%   INPUTS:
+% A is a NxN matrix representing a directed network
+%   A can be weighted (integer or non-integer)
+%   A(i,j) = # of dominance interactions by i toward j. 
+%   A(i,j) = # of times that j endorsed i.
+% s is the Nx1 vector of node positions (ranks)
+%   OUTPUTS:
+% b is the optimal inverse temperature (beta) under the LOCAL accuracy,
+%   which we call \sigma_a in the paper.
+
+function [b] = betaLocal(A,s)
+
+global M r
+M = A;
+r = s;
+% minimize -1*accuracy for some b between 1e-6 and 500:
+b = fminbnd(@negacc,1e-6,500);
+end
+
+function [a] = negacc(b)
+global M r
+m = sum(sum(M));
+n = length(r);
+y = 0;
+for i=1:n
+    for j=1:n
+        d = r(i) - r(j);
+        y = y + abs( M(i,j)- (M(i,j) + M(j,i))*((1+exp(-2*b*d))^(-1)) );
+    end
+end
+a = y/m-1;
+end
